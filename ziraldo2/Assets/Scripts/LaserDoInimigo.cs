288cccdcd41925da558aaf101a81be24f405ddf4
PeldL <gabrielgd341@gmail.com>
2024-09-23 20:25:18 -0300

    cuadadad

--- ziraldo2/Assets/Scripts/LaserDoInimigo.cs
+++ ziraldo2/Assets/Scripts/LaserDoInimigo.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserDoInimigo : MonoBehaviour
-{ 
-    public float velocidadeDoLaser;
+{
 
+    public float velocidadeDoLaser;
     public int danoParaDar;
-  
+
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -24,17 +25,13 @@ public class LaserDoInimigo : MonoBehaviour
         transform.Translate(Vector3.up * velocidadeDoLaser * Time.deltaTime);
     }
 
-
-
-    void OnTriggerEnter2D(Collider2D other)
+    void OntriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponente<VidaDoJogador>().MachucarJogador(danoParaDar);
+            other.gameObject.GetComponent<VidaDoJogador>().MachucarJogador(danoParaDar);
+            Destroy(this.gameObject);
         }
-
     }
 
-
-    
 }

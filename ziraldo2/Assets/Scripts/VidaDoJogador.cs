ae6120a3767d6f57fa0ff29d953d1e474753381e
PeldL <gabrielgd341@gmail.com>
2024-09-26 16:17:05 -0300

    aula 14 feita

--- ziraldo2/Assets/Scripts/VidaDoJogador.cs
+++ ziraldo2/Assets/Scripts/VidaDoJogador.cs
@@ -8,9 +8,16 @@ public class VidaDoJogador : MonoBehaviour
 
     public Slider barraDeVidaDoJogador;
 
+    public GameObject escudoDoJogador;
+
     public int vidaMaximaDoJogador;
 
     public int vidaAtualDoJogador;
+
+    public int vidaMaximaDoEscudo;
+
+    public int vidaAtualDoEscudo;
+
 
 
     public bool temEscudo;
@@ -22,29 +29,52 @@ public class VidaDoJogador : MonoBehaviour
         vidaAtualDoJogador = vidaMaximaDoJogador;
         barraDeVidaDoJogador.maxValue = vidaMaximaDoJogador;
         barraDeVidaDoJogador.value = vidaAtualDoJogador;
+
+        escudoDoJogador.SetActive(false);
+        temEscudo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void AtivarEscudo()
+    {
+        vidaAtualDoEscudo = vidaMaximaDoJogador;
+
+
+        escudoDoJogador.SetActive(true);
+        temEscudo = true;
+    }
+
 
     public void MachucarJogador(int danoParaReceber)
     {
         if (temEscudo == false)
+        {
             vidaAtualDoJogador -= danoParaReceber;
             barraDeVidaDoJogador.value = vidaAtualDoJogador;
 
+            if (vidaAtualDoJogador <= 0)
+            {
+                Debug.Log("Game Over");
+            }
+        }
+        else
+        {
+            vidaAtualDoEscudo -= danoParaReceber;
+            if (vidaAtualDoEscudo <=0)
+            {
+                escudoDoJogador.SetActive(false);
+                temEscudo = false;
+            }
 
 
-             if(vidaAtualDoJogador <= 0)
-             {
-            Debug.Log("Game Over");
-             }
 
+
+        }
     }
-
-
 
 }

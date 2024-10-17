1c2012a85dcda0ff7bf832a08027c34d9c667c84
caetanoXD <caetano.madeira@escola.pr.gov.br>
2024-10-17 16:39:28 -0300

    aula 28 quase completa

---
+++ ziraldo2/Assets/Scripts/PainelDeGameOver.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PainelDeGameOver : MonoBehaviour
+{
+  public void ReiniciarJogo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+        Debug.Log("Saiu do jogo");
+
+    }
+}

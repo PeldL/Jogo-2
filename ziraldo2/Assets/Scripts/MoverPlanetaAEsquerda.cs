e3a9465ccc3c02892d236f74a9ae316c5937fa24
caetanoXD <caetano.madeira@escola.pr.gov.br>
2024-10-21 08:26:09 -0300

    aula 29 feita

---
+++ ziraldo2/Assets/Scripts/MoverPlanetaAEsquerda.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPlanetaAEsquerda : MonoBehaviour
+{
+    public float velocidadeDoObjeto;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovimentarObjeto();
+    }
+    private void MovimentarObjeto()
+    {
+        transform.Translate(Vector3.down * velocidadeDoObjeto * Time.deltaTime);
+
+    }
+
+}
